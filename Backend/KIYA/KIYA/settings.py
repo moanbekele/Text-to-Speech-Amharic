1b4631fe93fa302c68a11f68e1c3d4fc6dc42459
MoanBekele <moanbekele@gmail.com>
2023-09-03 02:53:31 +0300

    Dawit

--- Backend/KIYA/KIYA/settings.py
+++ Backend/KIYA/KIYA/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +41,6 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'base',
-    'api',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +127,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add this at the end of the file
+if DEBUG:
+    # Static files (CSS, JavaScript, images)
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
